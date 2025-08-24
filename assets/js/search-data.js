1f925ec3bbe3b4075d7084899e0885381e96d736
yankaqiu <56242996+yankaqiu@users.noreply.github.com>
2025-08-24 15:13:59 +0000

    Deploying to gh-pages from @ yankaqiu/yankaqiu.github.io@3b10dc593030e108c2b1618ee80a27885858b77e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,17 +63,6 @@ ninja.data = [{
           window.location.href = "/blog/2023/jupyter-notebook/";
         
       },
-    },{id: "post-a-post-with-custom-blockquotes",
-      
-        title: "a post with custom blockquotes",
-      
-      description: "an example of a blog post with custom blockquotes",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/custom-blockquotes/";
-        
-      },
     },{id: "post-displaying-external-posts-on-your-al-folio-blog",
       
         title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
