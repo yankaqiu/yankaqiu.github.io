cf078397b7355ddc8e7c733f4396658e384e3c46
yankaqiu <56242996+yankaqiu@users.noreply.github.com>
2025-08-26 11:53:59 +0000

    Deploying to gh-pages from @ yankaqiu/yankaqiu.github.io@66d5d81bcf08bff4b70565d91167bdd97e4b5f20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,17 +52,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/%E8%BF%90%E5%8A%A8%E7%BB%99%E6%88%91%E5%B8%A6%E6%9D%A5%E4%BA%86%E4%BB%80%E4%B9%88/";
         
       },
-    },{id: "post-a-post-with-jupyter-notebook",
-      
-        title: "a post with jupyter notebook",
-      
-      description: "an example of a blog post with jupyter notebook",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/jupyter-notebook/";
-        
-      },
     },{id: "post-displaying-external-posts-on-your-al-folio-blog",
       
         title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
